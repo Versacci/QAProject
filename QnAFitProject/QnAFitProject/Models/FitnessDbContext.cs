c1f312a34d9df52a7570b060b55ffbf2125dcb9b
Versacci <ahmed_farah_2@hotmail.com>
2017-09-13 15:54:19 +0200

    Added Category
    Added Category crud with repository

---
+++ QnAFitProject/QnAFitProject/Models/FitnessDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace QnAFitProject.Models
+{
+    public class FitnessDbContext : ApplicationDbContext
+    {
+        public FitnessDbContext()
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
